ce91043be0162dd3a1466ee36044818d74911ff0
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-06-27 11:47:19 +0200

    create mainpage template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
 // import { persistor, store } from "./redux/store";
 // import { PersistGate } from "redux-persist/integration/react";
@@ -14,11 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <Provider store={store}>
   //   <PersistGate loading={null} persistor={persistor}>
-  <Provider>
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+
 );
 
 reportWebVitals();
