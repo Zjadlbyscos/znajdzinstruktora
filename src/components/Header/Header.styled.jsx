183b32a213f17fc85339a21df25407cb4ff3c088
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-06-27 13:41:31 +0200

    header template

---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center
+  
+  
+`
+export const StyleLogo=styled.div`
+    width: 25px;
+    height: 25px;
+    background-color: aqua;
+`
