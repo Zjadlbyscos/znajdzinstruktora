38962223876f142b87d08a9b2bff4c6982a08b94
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-06-28 14:26:14 +0200

    create useOutsideClick.js  hook

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+const useOutsideClick = (ref, callback, isActive) => {
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    };
+
+    if (isActive) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, callback, isActive]);
+};
+
+export default useOutsideClick;
