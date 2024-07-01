6ad9abb4b45d354314e028769dcba617f2f15a04
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-07-01 12:49:16 +0200

    fix bug ; replace class - className

--- src/components/RenderSvg/RenderSvg.jsx
+++ src/components/RenderSvg/RenderSvg.jsx
@@ -4,16 +4,16 @@ export function LogoHeader() {
 
       <g id="Warstwa_1-2" data-name="Warstwa 1">
         <g>
-          <rect class="cls-1" x="85.12" y="58.05" width="188.09" height="25" rx="12.5" ry="12.5" transform="translate(102.36 -106.02) rotate(45)"/>
-          <rect class="cls-1" x="161.87" y="26.85" width="98.99" height="25" rx="12.5" ry="12.5" transform="translate(89.73 -137.93) rotate(45)"/>
-          <rect class="cls-1" x="224.85" y="0" width="25.11" height="25.11" rx="12.55" ry="12.55" transform="translate(78.41 -164.2) rotate(45)"/>
+          <rect className="cls-1" x="85.12" y="58.05" width="188.09" height="25" rx="12.5" ry="12.5" transform="translate(102.36 -106.02) rotate(45)"/>
+          <rect className="cls-1" x="161.87" y="26.85" width="98.99" height="25" rx="12.5" ry="12.5" transform="translate(89.73 -137.93) rotate(45)"/>
+          <rect className="cls-1" x="224.85" y="0" width="25.11" height="25.11" rx="12.55" ry="12.55" transform="translate(78.41 -164.2) rotate(45)"/>
         </g>
         <g>
-          <rect class="cls-2" x="-23.18" y="167.66" width="188.09" height="25" rx="12.5" ry="12.5" transform="translate(-6.42 357.66) rotate(-135)"/>
-          <rect class="cls-2" x="-10.84" y="198.87" width="98.99" height="25" rx="12.5" ry="12.5" transform="translate(-83.46 388.16) rotate(-135)"/>
-          <rect class="cls-2" x=".07" y="225.61" width="25.11" height="25.11" rx="12.55" ry="12.55" transform="translate(-146.86 415.49) rotate(-135)"/>
+          <rect className="cls-2" x="-23.18" y="167.66" width="188.09" height="25" rx="12.5" ry="12.5" transform="translate(-6.42 357.66) rotate(-135)"/>
+          <rect className="cls-2" x="-10.84" y="198.87" width="98.99" height="25" rx="12.5" ry="12.5" transform="translate(-83.46 388.16) rotate(-135)"/>
+          <rect className="cls-2" x=".07" y="225.61" width="25.11" height="25.11" rx="12.55" ry="12.55" transform="translate(-146.86 415.49) rotate(-135)"/>
         </g>
-        <rect class="cls-2" x="76.81" y="112.14" width="98.99" height="25" rx="12.5" ry="12.5" transform="translate(303.76 123.46) rotate(135)"/>
+        <rect className="cls-2" x="76.81" y="112.14" width="98.99" height="25" rx="12.5" ry="12.5" transform="translate(303.76 123.46) rotate(135)"/>
       </g>
     </svg>
     );
