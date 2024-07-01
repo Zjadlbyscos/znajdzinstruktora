eda66f7caa00bb2788596d45b5f8a55b2e408019
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-07-01 16:02:33 +0200

    style : add underline :hoover

--- src/components/Header/UserModal/UserModal.styled.jsx
+++ src/components/Header/UserModal/UserModal.styled.jsx
@@ -12,4 +12,27 @@ gap: 25px;
   padding: 23px 5px;
   border-radius: 25px;
 
+
+a {
+  position: relative;
+}
+
+  a::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 1px;
+  border-radius: 4px;
+  background-color: var( --primary-color);
+  bottom: 0;
+  left: 0;
+  transform-origin: right;
+  transform: scaleX(0);
+  transition: transform .2s ease-in-out;
+} 
+
+a:hover::before {
+  transform-origin: left;
+  transform: scaleX(1);
+}
 `
