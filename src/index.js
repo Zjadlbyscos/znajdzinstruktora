c6748b51a819e4ca0e31c7c85c0d255817c76189
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-07-01 09:27:57 +0200

    Configured redux store

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
-// import { persistor, store } from "./redux/store";
-// import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import App from "./App";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <Provider store={store}>
-  //   <PersistGate loading={null} persistor={persistor}>
-
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
 
 reportWebVitals();
