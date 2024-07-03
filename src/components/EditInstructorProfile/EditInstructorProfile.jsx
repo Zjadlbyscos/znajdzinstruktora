8b7f52c5b080942daccf6a2d7887ba235fab8e2a
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-07-03 15:16:19 +0200

    add editProfile component & render

---
+++ src/components/EditInstructorProfile/EditInstructorProfile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ChangePassword from "./ChangePassword/ChangePassword";
+
+export const InstructorProfile = () => {
+    return (
+<>
+
+
+
+<p>ChangePassword.jsx  </p>
+<ChangePassword/>
+</>
+    );
+  };
---
+++ src/pages/EditInstructorProfile.jsx
@@ -0,0 +1,8 @@
+import { InstructorProfile } from "../components/EditInstructorProfile/EditInstructorProfile";
+export const EditInstructorProfile = () => {
+    return (
+      <section>
+ <InstructorProfile/>
+      </section>
+    );
+  };
