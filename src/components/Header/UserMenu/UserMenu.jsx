328c33cf0ecc7c48e2a90fce6d3cc22a8cbcbafd
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-07-15 10:17:42 +0200

    Nav links, register communicate
    Changed user modal nav links depending on if user is logged in. If it is it changes to "edytuj profil" and "wyloguj".
    
    After successfully registered communicate appears above the form.

--- src/components/Header/UserMenu/UserMenu.jsx
+++ src/components/Header/UserMenu/UserMenu.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { selectIsLoggedIn } from "../../../redux/auth/selectors";
 
 export const UserMenu = () => {
-  
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  console.log("isLoggedIn", isLoggedIn);
+
   return (
     <>
-      <NavLink to="/czyInstruktor">Zarejestruj</NavLink>
-      <NavLink to="/zaloguj">Zaloguj</NavLink>
+      {!isLoggedIn ? (
+        <>
+          <NavLink to="/czyInstruktor">Zarejestruj</NavLink>
+          <NavLink to="/zaloguj">Zaloguj</NavLink>
+        </>
+      ) : (
+        <>
+          <NavLink to="/edytuj-profil">Edytuj profil</NavLink>
+          <NavLink to="/wyloguj">Wyloguj</NavLink>
+        </>
+      )}
     </>
   );
 };
