efbb3f32f44a1ce7e6a4ea9dcd57c98b1a790c78
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-08-08 19:34:34 +0200

    editProfileConfig
    Modified editProfileConfig function to return arrays of classLevels and languages

--- src/hooks/editProfileConfig.js
+++ src/hooks/editProfileConfig.js
@@ -1,30 +1,25 @@
 export const editProfileConfig = () => {
-  const classLevel = [
-    {
-      label: "rodzaj zajęc",
-      name: "classes",
-      type: "checkbox",
-      options: [
-        "Sekcja Początkująca",
-        "Sekcja Średniozaawansowana",
-        "Sekcja Zaawansowana",
-        "Sekcja dla dzieci",
-        "Sekcja dla młodzieży 11-14 lat,",
-        "Sekcja dla młodzieży 14-18lat",
-        "Zajęcia Indywidualne",
-        "Dla osób z niepełnosprawnościami",
-        "Plan Treningowy",
-      ],
-    },
-  ];
-
-  const languages = [
-    {
-      label: "język",
-      name: "languages",
-      type: "checkbox",
-      options: ["Polski", "Angielski", "Niemiecki", "Francuski"],
-    },
-  ];
-  return { classLevel, languages };
+  return {
+    classLevel: [
+      "Sekcja Początkująca",
+      "Sekcja Średniozaawansowana",
+      "Sekcja Zaawansowana",
+      "Sekcja dla dzieci",
+      "Sekcja dla młodzieży 11-14 lat,",
+      "Sekcja dla młodzieży 14-18lat",
+      "Zajęcia Indywidualne",
+      "Dla osób z niepełnosprawnościami",
+      "Plan Treningowy",
+    ],
+    languages: [
+      "Polski",
+      "Angielski",
+      "Niemiecki",
+      "Francuski",
+      "Włoski",
+      "Hiszpański",
+      "Ukraiński",
+      "Rosyjski",
+    ],
+  };
 };
