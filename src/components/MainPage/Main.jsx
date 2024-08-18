1eea652e3fcbc2054458a55dfdebbbb4cbeef8e1
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2024-08-18 13:17:31 +0200

    prettier

--- src/components/MainPage/Main.jsx
+++ src/components/MainPage/Main.jsx
@@ -1,20 +1,17 @@
-import { Background, Wrapper , Content,Image} from "./Main.styled"
+import { Background, Wrapper, Content, Image } from "./Main.styled";
 
-export const Main = ()=>{
-    return(
-        <Background>
+export const Main = () => {
+  return (
+    <Background>
       <section>
         <Wrapper>
-            <Content>
-            <p>ZNAJDŹ ZAJĘCIA DLA SIEBIE 
-           </p>
-<p>     CONTENT MAIN.JSX</p>
-            </Content>
-            <Image></Image>
+          <Content>
+            <p>ZNAJDŹ ZAJĘCIA DLA SIEBIE</p>
+            <p> CONTENT MAIN.JSX</p>
+          </Content>
+          <Image></Image>
         </Wrapper>
-
       </section>
-      </Background>
-
-    )
-   }
+    </Background>
+  );
+};
--- src/components/MainPage/Main.styled.jsx
+++ src/components/MainPage/Main.styled.jsx
@@ -8,42 +8,40 @@ export const Background = styled.div`
     rgba(97, 46, 125, 1) 31%,
     rgba(50, 17, 86, 1) 74%
   );
-  min-height: 80vh;
+  min-height: 85vh;
   height: 100%;
 `;
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column-reverse;
+  align-items: center;
   height: 80vh;
 
   @media screen and (min-width: 768px) {
-   
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
   }
 `;
 
-export const Content=styled.div`
-
- color   :white ;
- font-size: 23px;
- font-weight: 600;
- 
-`
-export const Image=styled.div`
-   margin-top: -30px;
-     background-image: url(${tlo_telefon});
-     background-repeat: no-repeat; 
-     width:100%;
-  height: 90%;
+export const Content = styled.div`
+  color: white;
+  font-size: 23px;
+  font-weight: 600;
+`;
+export const Image = styled.div`
+  margin-top: -30px;
+  background-image: url(${tlo_telefon});
+  background-repeat: no-repeat;
+  background-position: center center;
+  width: 100%;
+  height: 100%;
   background-size: contain;
   @media screen and (min-width: 768px) {
-    margin-top: -50px;
-    width:50%;
+    width: 50%;
     background-image: url(${tlo_komputer});
-    background-size: contain;
-    background-position: center top;
+    background-size: cover;
+    max-width: 700px;
   }
-`
+`;
