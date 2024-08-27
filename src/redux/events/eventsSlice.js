7e5d5bd468e74268d241623631aa17e2ba4e1f3b
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-08-27 20:58:59 +0200

    Events redux
    Configured redux for events operations.

---
+++ src/redux/events/eventsSlice.js
@@ -0,0 +1,63 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  createNewEvent,
+  deleteEvent,
+  fetchEventById,
+  fetchEvents,
+  fetchInstructorEvents,
+} from "./operations";
+
+const isPendingAction = (action) => {
+  return action.type.endsWith("/pending");
+};
+
+const isRejectAction = (action) => {
+  return action.type.endsWith("/rejected");
+};
+
+const handlePending = (state) => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
+
+const initialState = {
+  events: [],
+  event: {},
+  instructorEvents: [],
+  isLoading: false,
+  error: null,
+};
+
+const eventsSlice = createSlice({
+  name: "events",
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchEvents.fulfilled, (state, action) => {
+        state.events = action.payload;
+      })
+      .addCase(fetchInstructorEvents.fulfilled, (state, action) => {
+        state.instructorEvents = action.payload;
+      })
+      .addCase(fetchEventById.fulfilled, (state, action) => {
+        state.event = action.payload;
+      })
+      .addCase(createNewEvent.fulfilled, (state, action) => {
+        state.event = action.payload;
+      })
+      .addCase(deleteEvent.fulfilled, (state, action) => {
+        state.event = action.payload;
+      })
+      .addMatcher(isPendingAction, handlePending)
+      .addMatcher(isRejectAction, handleRejected)
+      .addDefaultCase((state) => {
+        state.error = "someone use old function, fix it!";
+      });
+  },
+});
+
+export const eventsReducer = eventsSlice.reducer;
