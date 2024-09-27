f36203c3aa91469b0b5c744555ee106db1bbcb48
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-09-27 17:35:02 +0200

    ViewInstructorProfile
    Created ViewInstructorProfile component with a user view on it. Created styles aswell.

--- src/components/Insctuctors/ViewInstructorProfile/ViewInstructorProfile.jsx
+++ src/components/Insctuctors/ViewInstructorProfile/ViewInstructorProfile.jsx
@@ -1,12 +1,57 @@
+import { useEffect } from "react";
 import {
+  BioContainer,
+  Contact,
+  ContactContainer,
+  ContactHeader,
   ImageContainer,
   InstructorProfileWrapper,
+  Profile,
+  SocialsContainer,
 } from "./ViewInstructorProfile.styled";
+import { useDispatch, useSelector } from "react-redux";
+import { getInstructorById } from "../../../redux/instructors/operations";
+import { useParams } from "react-router-dom";
+import { selectInstructor } from "../../../redux/instructors/selectors";
+import { EnvelopeSVG, FBSvg, IGSvg, PhoneSVG } from "../../RenderSvg/RenderSvg";
+import { SocialMedia } from "./Socials/Socials";
 
 export const ViewInstructorProfile = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const instructor = useSelector(selectInstructor);
+
+  useEffect(() => {
+    dispatch(getInstructorById(id));
+  }, [dispatch, id]);
+
   return (
     <InstructorProfileWrapper>
-      <ImageContainer></ImageContainer>
+      <Profile>
+        <ImageContainer>
+          <img src={instructor.image} alt="instructor" />
+          <h2>{instructor.fullName}</h2>
+        </ImageContainer>
+        <BioContainer>
+          <p>{instructor.bio}</p>
+        </BioContainer>
+        <ContactHeader>Kontakt</ContactHeader>
+        <ContactContainer>
+          <Contact>
+            <a href={`tel:${instructor.phoneNumber}`}>
+              <PhoneSVG />
+              {instructor.phoneNumber}
+            </a>
+            <a href={`mailto:${instructor.email}`}>
+              <EnvelopeSVG />
+              {instructor.email}
+            </a>
+          </Contact>
+          <SocialsContainer>
+            <SocialMedia instructor={instructor} />
+          </SocialsContainer>
+        </ContactContainer>
+      </Profile>
     </InstructorProfileWrapper>
   );
 };
--- src/components/Insctuctors/ViewInstructorProfile/ViewInstructorProfile.styled.jsx
+++ src/components/Insctuctors/ViewInstructorProfile/ViewInstructorProfile.styled.jsx
@@ -1,5 +1,63 @@
 import styled from "styled-components";
 
-export const InstructorProfileWrapper = styled.div``;
+export const InstructorProfileWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export const ImageContainer = styled.div``;
+export const ImageContainer = styled.div`
+  margin-top: 20px;
+  img {
+    border-radius: 15%;
+  }
+`;
+
+export const Profile = styled.div`
+  text-align: center;
+  width: 75%;
+  background-color: var(--background-2);
+  border-radius: 8px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+  margin-top: 50px;
+`;
+
+export const BioContainer = styled.div`
+  width: 80%;
+  margin: 20px auto;
+`;
+
+export const ContactContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px 18px;
+`;
+export const Contact = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  a {
+    display: flex;
+    align-items: center;
+  }
+  svg {
+    width: 23px;
+    height: 23px;
+    margin-right: 10px;
+    &:hover {
+      opacity: 0.7;
+      transition: opacity 0.3s;
+    }
+  }
+`;
+
+export const SocialsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 9px;
+`;
+
+export const ContactHeader = styled.h3`
+  text-align: left;
+  margin-left: 18px;
+`;
