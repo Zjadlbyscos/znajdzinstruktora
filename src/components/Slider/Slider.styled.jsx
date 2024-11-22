bb1243059b28d06a04ba0bcfc82a67075d62e626
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-11-22 20:07:33 +0100

    Create Slider.styled.jsx
    Moved slider styles into one component

---
+++ src/components/Slider/Slider.styled.jsx
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export const StyledSlider = styled(Slider)`
+  .slick-list {
+    margin: 0 auto;
+    padding: 0 10px;
+  }
+  .slick-track {
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+  }
+  .slick-prev,
+  .slick-next {
+    top: 100%;
+    transform: translateY(-50%);
+    width: 28px;
+    height: 28px;
+    background-color: var(--button-primary);
+    @media (min-width: 680px) {
+      width: 35px;
+      height: 35px;
+    }
+    @media (min-width: 1024px) {
+      width: 40px;
+      height: 40px;
+    }
+
+    .--fc-more-link-text-color {
+      color: white;
+    }
+  }
+  .slick-prev {
+    left: calc(10% - 10px);
+  }
+  .slick-next {
+    right: calc(10% - 10px);
+  }
+  @media (min-width: 680px) {
+    .slick-prev {
+      left: calc(10% - 15px);
+    }
+    .slick-next {
+      right: calc(10% - 40px);
+    }
+  }
+  @media (min-width: 1024px) {
+    .slick-prev {
+      left: calc(5% - 35px);
+    }
+    .slick-next {
+      right: calc(5% - 70px);
+    }
+  }
+  .slick-arrow {
+    margin: 30px 0;
+  }
+  .slick-prev.slick-disabled::before,
+  .slick-next.slick-disabled::before {
+    opacity: 1;
+  }
+  .slick-next::before,
+  .slick-prev::before {
+    @media (min-width: 1024px) {
+      font-size: 25px;
+    }
+  }
+`;
