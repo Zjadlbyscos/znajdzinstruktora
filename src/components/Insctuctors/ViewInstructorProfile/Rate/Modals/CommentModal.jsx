15941cb90b7fdd7d2e404197144474779ff7e600
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-12-08 08:15:31 +0100

    CommentModal
    Styled CommentModal

--- src/components/Insctuctors/ViewInstructorProfile/Rate/Modals/CommentModal.jsx
+++ src/components/Insctuctors/ViewInstructorProfile/Rate/Modals/CommentModal.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Comment,
+  CommentContainer,
+  CommentModalWrapper,
+} from "./CommentModal.styled";
 
-export const CommentModal = ({ handleClose, comment }) => {
+export const CommentModal = ({ handleClose, rate, date }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true);
+    return () => {
+      setIsVisible(false);
+    };
+  }, []);
+  const formattedDate = new Date(rate.createdAt).toLocaleDateString();
   return (
-    <div>
-      <div>
-        <button onClick={handleClose}>Zamknij</button>
-        <p>{comment}</p>
-      </div>
-    </div>
+    <CommentModalWrapper className={isVisible ? "visible" : "hidden"}>
+      <button onClick={handleClose}>X</button>
+      <CommentContainer>
+        <p>{rate.rating}</p>
+        <p>{rate.userFullName}</p>
+        <p>{formattedDate}</p>
+        <Comment>{rate.comment}</Comment>
+      </CommentContainer>
+    </CommentModalWrapper>
   );
 };
