0286b8ae8b67245d455e9e1c82d1b3cdee2c7f87
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2025-02-09 18:26:53 +0100

    Instructors
    Styled instructors cards in gallery element component. Changed value of limit in fecthInstructors function

--- src/components/GalleryElement/GalleryElement.styled.jsx
+++ src/components/GalleryElement/GalleryElement.styled.jsx
@@ -7,25 +7,17 @@ export const GalleryElementUl = styled.ul`
   list-style: none;
   padding: 0;
   justify-content: center;
-  margin-top: 30px;
 `;
 
 export const GalleryImg = styled.img`
   display: block;
-  width: 189px;
-  max-width: 100%;
-  height: 270px;
+  width: 100%;
   object-fit: cover;
   border-radius: 15px;
 `;
 
 export const StyledElement = styled.li`
   cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 30px;
   &:hover {
     transition: 250ms;
     transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
@@ -50,13 +42,31 @@ export const StyledCaption = styled.div`
   background-color: rgba(158, 107, 132, 0.7);
   color: white;
   text-align: center;
+  @media screen and (min-width: 768px) {
+  }
+  p {
+    font-size: 14px;
+    font-weight: 600;
+    @media screen and (min-width: 768px) {
+      font-size: 18px;
+    }
+  }
 `;
 
 export const StyledBio = styled.p`
   color: var(--text-color-white);
-  padding: 20px 24px;
+  padding: 20px 36px;
   position: absolute;
   transform: translateY(100%);
   opacity: 0;
   transition: transform 250ms ease-in-out, opacity 250ms ease-in-out;
+  font-size: 14px;
+  @media screen and (min-width: 768px) {
+    padding: 16px 24px;
+    font-size: 12px;
+  }
+  @media screen and (min-width: 1024px) {
+    padding: 20px 36px;
+    font-size: 18px;
+  }
 `;
